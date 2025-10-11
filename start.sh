0fb3775ac14c48ff9c638f50eaa9153afc194300
Atr-eus <an1ndya@proton.me>
2025-10-12 00:33:37 +0600

    basic structure of the bash script

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+# INSTALL DEPTS
 # if ! pacman -Q grim slurp imagemagick manga-ocr-git &> /dev/null; then
 if ! pacman -Q grim slurp imagemagick &> /dev/null; then
   if command -v pacman &> /dev/null && command -v doas &> /dev/null; then
@@ -14,3 +15,21 @@ if ! pacman -Q grim slurp imagemagick &> /dev/null; then
   else
     printf "[WORKER] Required packges are already installed.\n\n"
 fi
+
+# PARAMETERS
+WEBLIVE="$PWD/index.html"
+OUTFILE="$PWD/out.txt"
+TEMPIMG="/tmp/fumiko-$(uuidgen).png"
+
+# SAVE THE SCREENSHOT
+grim -g "$(slurp -d)" "$TEMPIMG" || exit 1;
+
+# RUN MANGA-OCR ON THE TAKEN SS
+out="$(manga_ocr "$TMPIMG" || python -m manga_ocr "$TMPIMG" || true)"
+out="$(echo "$text" | sed '/^\s*$/d')"
+
+# printf "$out"
+# WRITE OUTPUT TEXT TO FILE (TO BE SERVED BY A WEB SERVER)
+tmpout="$(mktemp "${OUTFILE}.tmp.XXXXXX")"
+printf '%s\n' "$text" > "$tmpout"
+mv "$tmpout" "$OUTFILE"
